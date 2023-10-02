a0da5ce269d266c20dc68564bc0404a7fdf27690
Ahmed Kasu <ankassu@gmail.com>
2023-10-02 16:13:20 +0300

    added a blog model

---
+++ src/models/Blog.js
@@ -0,0 +1,38 @@
+import { Model, DataTypes } from 'sequelize'
+import sequelize from '../db.js'
+
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.STRING(50),
+    },
+    url: {
+      type: DataTypes.STRING(500),
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog',
+  }
+)
+
+Blog.sync()
+
+export default Blog
