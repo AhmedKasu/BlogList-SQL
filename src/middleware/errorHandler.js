c270c9336e2a0cc9440c9df79c591791b30c157f
Ahmed Kasu <ankassu@gmail.com>
2023-10-03 13:34:23 +0300

    added an errorHandler middleware

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,32 @@
+const errorHandler = (error, _req, res, _next) => {
+  const { name, message, details, status } = error
+  switch (name) {
+    case 'SequelizeDatabaseError':
+    case 'SequelizeValidationError':
+    case 'SequelizeUniqueConstraintError':
+      return res.status(400).json({
+        error: name,
+        details: message,
+      })
+
+    case 'ValidationError':
+      return res.status(status).json({
+        error: name,
+        details: message ? message : details,
+      })
+
+    case 'NotFoundError':
+      return res.status(status).json({
+        error: name,
+        details: message ? message : 'Resource not found!',
+      })
+
+    default:
+      return res.status(500).json({
+        error: 'Unknown error',
+        details: message,
+      })
+  }
+}
+
+export default errorHandler
