9613dff3a6826da256ec6617ad9b6088fc60578a
Ahmed Kasu <ankassu@gmail.com>
2023-10-06 13:56:54 +0300

    added JWT_SECRET env variable

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,5 +4,6 @@ dotenv.config()
 
 const PORT = process.env.PORT || 5000
 const DATABASE_URL_DEV = process.env.DATABASE_URL_DEV
+const JWT_SECRET = process.env.JWT_SECRET
 
-export { PORT, DATABASE_URL_DEV }
+export { PORT, DATABASE_URL_DEV, JWT_SECRET }
