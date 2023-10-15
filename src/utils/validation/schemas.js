3c94c27113f673b87b557b5775eee7666876f997
Ahmed Kasu <ankassu@gmail.com>
2023-10-15 14:45:19 +0300

    added custom error messages to validation schemas

--- src/utils/validation/schemas.js
+++ src/utils/validation/schemas.js
@@ -1,25 +1,46 @@
 import { z } from 'zod'
 
 const blogSchema = z.object({
-  author: z.string().max(50),
-  url: z.string().url().nonempty(),
-  title: z.string().max(50).nonempty(),
-  likes: z.number().int().default(0),
+  author: z
+    .string()
+    .max(50, "Author's name should be no more than 50 characters"),
+  url: z.string().url('Invalid URL format').nonempty('URL cannot be empty'),
+  title: z
+    .string()
+    .max(50, 'Title should be no more than 50 characters')
+    .nonempty('Title cannot be empty'),
+  likes: z.number().int('Likes must be an integer').default(0),
 })
 
-const blogIdSchema = z.string().refine((value) => {
-  const reg = /^\d+$/
-  return reg.test(value)
-})
+const blogIdSchema = z
+  .string()
+  .nonempty('Blog ID cannot be empty')
+  .refine((value) => {
+    const reg = /^\d+$/
+    return reg.test(value)
+  }, 'Blog ID must be a positive integer')
 
 const likesUpdateSchema = z.object({
-  likes: z.number().int().min(1),
+  likes: z
+    .number()
+    .int('Likes must be an integer')
+    .min(1, 'Minimum likes must be at least 1'),
 })
 
 const userSchema = z.object({
-  username: z.string().min(3).max(30).nonempty(),
-  name: z.string().max(30).nonempty(),
-  password: z.string().min(5).max(30),
+  username: z
+    .string()
+    .min(3, 'Username should be at least 3 characters')
+    .max(30, 'Username should be no more than 30 characters')
+    .nonempty('Username cannot be empty'),
+  name: z
+    .string()
+    .max(30, 'Name should be no more than 30 characters')
+    .nonempty('Name cannot be empty'),
+  password: z
+    .string()
+    .min(5, 'Password should be at least 5 characters')
+    .max(30, 'Password should be no more than 30 characters'),
 })
 
 const usernameSchema = z.object({
