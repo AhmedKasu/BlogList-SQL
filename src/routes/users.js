05345862062282b37f9da6bacba2ab95593104f6
Ahmed Kasu <ankassu@gmail.com>
2023-10-17 14:41:04 +0300

    task 13.20: added a single user route, returning user's information & reading list

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,11 +4,17 @@ import _ from 'lodash'
 
 import { User, Blog } from '../models/index.js'
 import findByUsername from '../middleware/findByUsername.js'
+import findById from '../middleware/findById.js'
 import auth from '../middleware/auth.js'
-import { userSchema, usernameSchema } from '../utils/validation/schemas.js'
+import {
+  idSchema,
+  userSchema,
+  usernameSchema,
+} from '../utils/validation/schemas.js'
 import validateUserInput from '../utils/validation/index.js'
 
 const router = Router()
+const singleUser = Router()
 
 router.post('/', async (req, res) => {
   const validatedUser = validateUserInput(userSchema, req.body)
@@ -38,5 +44,29 @@ router.put('/:username', auth, findByUsername, async (req, res) => {
   await req.user.save()
   res.status(200).send(_.omit(req.user.toJSON(), ['password']))
 })
+
+singleUser.get('/', auth, async (req, res) => {
+  res.status(200).json(req.user)
+})
+
+const singleUserQueryOptions = {
+  attributes: { exclude: ['password', 'createdAt', 'updatedAt'] },
+  include: {
+    model: Blog,
+    as: 'readings',
+    attributes: {
+      exclude: ['userId', 'createdAt', 'updatedAt', 'readinglist'],
+    },
+    through: {
+      attributes: [],
+    },
+  },
+}
+
+router.use(
+  '/:id',
+  findById(User, 'user', idSchema, singleUserQueryOptions),
+  singleUser
+)
 
 export default router
