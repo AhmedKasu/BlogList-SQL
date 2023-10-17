3791acd2d9d1e7d98325d59962e8dd10cc2ab3bd
Ahmed Kasu <ankassu@gmail.com>
2023-10-17 14:35:24 +0300

    extended findById middleware to take queryOptions argument

--- src/middleware/findById.js
+++ src/middleware/findById.js
@@ -1,10 +1,11 @@
 import validateUserInput from '../utils/validation/index.js'
 import { NotFoundError } from '../utils/errors.js'
 
-const findById = (Model, resource, validationSchema) => {
+const findById = (Model, resource, validationSchema, queryOptions = {}) => {
   return async (req, _res, next) => {
     req[resource] = await Model.findByPk(
-      validateUserInput(validationSchema, req.params.id)
+      validateUserInput(validationSchema, req.params.id),
+      queryOptions
     )
 
     if (!req[resource]) throw new NotFoundError(`${resource} not found!`)
