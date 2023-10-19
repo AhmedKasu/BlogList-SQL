a8594b603945005a86b8d81770ad3f68c5edd271
Ahmed Kasu <ankassu@gmail.com>
2023-10-19 17:15:04 +0300

    task 13.24: extended the auth middleware to check for session validity

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,11 +1,26 @@
 import { JWT_SECRET } from '../utils/config.js'
 import jwt from 'jsonwebtoken'
 
+import { Session } from '../models/index.js'
+
 const auth = async (req, res, next) => {
   const authorization = req.get('authorization')
+
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    const undecodedToken = authorization.substring(7)
+
     try {
-      req.authUser = jwt.verify(authorization.substring(7), JWT_SECRET)
+      const decodedToken = jwt.verify(undecodedToken, JWT_SECRET)
+
+      const validSession = await Session.findOne({
+        where: { token: undecodedToken },
+      })
+
+      if (!validSession)
+        return res.status(401).json({ error: 'session invalid or expired' })
+
+      req.authUser = decodedToken
+      req.session = validSession
     } catch {
       return res.status(401).json({ error: 'token invalid' })
     }
